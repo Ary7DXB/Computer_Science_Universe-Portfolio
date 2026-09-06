a0f6943c2126d8a90d84d546535687591f0f6762
Aryan Satghare <aryansatghare@hotmail.com>
2026-09-06 17:51:16 +0400

    Change texture element position from prepend to append

--- js/03-planet-focus.js
+++ js/03-planet-focus.js
@@ -62,8 +62,7 @@ function buildFocusClone(p){
   clone.innerHTML=`${rings}<div class="focus-glow"></div><div class="focus-sphere"><div class="focus-track"><img src="${p.texture}" alt="" draggable="false"><img src="${p.texture}" alt="" draggable="false"></div>${clouds}<div class="focus-grade"></div><div class="focus-light"></div><div class="focus-atmosphere"></div></div>${frontRing}`;
   if((p.spinDir||1)<0){
     const track=clone.querySelector('.focus-track'),texture=track.firstElementChild.cloneNode(true);
-    texture.style.position='absolute';texture.style.left='-50%';
-    track.prepend(texture);
+    track.appendChild(texture);
   }
   clone.style.setProperty('--focus-glow',p.glow);
   clone.style.setProperty('--focus-rotation',p.focusRotation||p.rotation||'24s');
